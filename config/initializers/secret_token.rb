5af476b8199cf8f9438c81b84f6335ad6f4813c9
Renato Ramiro <renato.ipj@gmail.com>
2011-03-22 11:59:46 -0300

    Minha loja virtual

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minhaloja::Application.config.secret_token = 'ef9f27e3f362f5c9050c6953252687434de76e36c8862f5245e1a998219baa96c795ea81f5f291b317bbdfa71fdf805e5101f33ff964c2d34386ec47056dd7c3'
